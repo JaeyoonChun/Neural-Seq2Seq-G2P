d878d3706cae999cc28b9242d981ae05b0a141b8
Jaeyoon <o0ococ@u.sogang.ac.kr>
2020-08-25 23:35:52 +0900

    change BucketIterator to Iterator

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,6 @@
 import torchtext.data as data
 from torchtext.data import Field, BucketIterator, Iterator
-from utils import get_file
+from utils import get_sent_file
 import os
 
 class Librispeech(data.Dataset):    
@@ -55,22 +55,39 @@ class DataLoader:
         self.train_data, self.val_data, self.test_data = self.librispeech.splits(fpath, self.G_FIELD, self.P_FIELD)
         self.batch_size = batch_size
         self.device = device
-        # print(vars(self.train_data.examples[0]))
+        print(vars(self.train_data.examples[0]))
+        print(vars(self.val_data.examples[0]))
+
         self.G_FIELD.build_vocab(self.train_data, self.val_data, self.test_data)
         self.P_FIELD.build_vocab(self.train_data, self.val_data, self.test_data)
-
+        # print(self.G_FIELD.vocab.stoi)
+        # print(self.P_FIELD.vocab.stoi)
     
     def load_dataset(self, fpath):
         for type in 'train dev test'.split():
-            get_file(fpath, type)
+            get_sent_file(fpath, type)
 
 
     def build_iterator(self):
-        train_iter, val_iter = BucketIterator.splits(
-            (self.train_data, self.val_data),
-            batch_size=self.batch_size,
-            device=self.device
-        )
+        train_iter = Iterator(self.train_data, batch_size=self.batch_size, train=True, device=self.device, shuffle=False)
+        val_iter = Iterator(self.val_data, batch_size=self.batch_size, train=False, device=self.device, shuffle=False)
         test_iter = Iterator(self.test_data, batch_size=1, train=False, device=self.device)
+
+        # batch = next(iter(train_iter))
+        # print(batch.grapheme)
+        # print(batch.grapheme.shape)
+        # print(batch.phoneme)
+        # print(batch.phoneme.shape)
+        # print(' '.join([self.G_FIELD.vocab.itos[i] for i in batch.grapheme.view(-1).numpy()]))
+        # print(' '.join([self.P_FIELD.vocab.itos[i] for i in batch.phoneme.view(-1).numpy()]))       
+
+        # batch = next(iter(val_iter))
+        # print(batch.grapheme)
+        # print(batch.grapheme.shape)
+        # print(batch.phoneme)
+        # print(batch.phoneme.shape)
+        # print(' '.join([self.G_FIELD.vocab.itos[i] for i in batch.grapheme.view(-1).numpy()]))
+        # print(' '.join([self.P_FIELD.vocab.itos[i] for i in batch.phoneme.view(-1).numpy()]))     
+        # raise Exception('dd')
         return (train_iter, val_iter, test_iter)
 
