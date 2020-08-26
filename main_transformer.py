d2323e186e5f1dfc94d2f0e1f3249b3d84e8de70
Jaeyoon <o0ococ@u.sogang.ac.kr>
2020-08-27 01:46:19 +0900

    add initialize weight

--- main_transformer.py
+++ main_transformer.py
@@ -21,8 +21,8 @@ def build_model(dataset, device):
     INPUT_DIM = len(dataset.G_FIELD.vocab)
     OUTPUT_DIM = len(dataset.P_FIELD.vocab)
     HID_DIM = 256
-    ENC_LAYERS = 6
-    DEC_LAYERS = 6
+    ENC_LAYERS = 3
+    DEC_LAYERS = 3
     ENC_HEADS = 8
     DEC_HEADS = 8
     ENC_PF_DIM = 512
@@ -36,7 +36,12 @@ def build_model(dataset, device):
     G_PAD_IDX = dataset.G_FIELD.vocab.stoi[dataset.G_FIELD.pad_token]
     P_PAD_IDX = dataset.P_FIELD.vocab.stoi[dataset.P_FIELD.pad_token]
     model = Seq2Seq(enc, dec, G_PAD_IDX, P_PAD_IDX, device)
-    
+
+    def initialize_weights(m):
+        if hasattr(m, 'weight') and m.weight.dim() > 1:
+            nn.init.kaiming_uniform_(m.weight.data)
+    model.apply(initialize_weights)
+
     def count_parameters(model):
         return sum(p.numel() for p in model.parameters() if p.requires_grad)
     print(f'The model has {count_parameters(model):,} trainable parameters')
@@ -48,10 +53,11 @@ def main(fpath, batch_size, mode, model_type, model_path):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     dataset = DataLoader(fpath, Librispeech, batch_size, device, model_type)
+
     model = build_model(dataset, device).to(device)
 
     if mode == 'train':
-        train(dataset, model, 100, model_type)
+        train(dataset, model, 500, model_type)
     if mode == 'test':
         test(dataset, model, device, model_path)
     if mode == 't':
@@ -59,5 +65,5 @@ def main(fpath, batch_size, mode, model_type, model_path):
 
 
 if __name__ == '__main__':
-    model_path = 'checkpoints/2020-08-25T19:18:53_transformer'
+    model_path = 'checkpoints/2020-08-26T23:36:19_transformer'
     main('data/', 32, 'train', 'transformer', model_path)
