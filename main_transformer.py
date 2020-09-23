b7419c9157f54734d22a0cc4172901aef30b234c
Jaeyoon chun <o0ococ@u.sogang.ac.kr>
2020-09-23 21:12:02 +0900

    add argparse

--- main_transformer.py
+++ main_transformer.py
@@ -4,33 +4,27 @@ import numpy as np
 import random
 import os
 
-from Train_transformer import train, test_temp
+from Train_transformer import train
 from Test_transformer import test
 from transformer import Encoder, Decoder, Seq2Seq
 from data_loader import Librispeech, DataLoader
-
-SEED = 1234
-random.seed(SEED)
-np.random.seed(SEED)
-torch.manual_seed(SEED)
-torch.cuda.manual_seed(SEED)
-torch.backends.cudnn.deterministic = True
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+from utils import init_logger, set_seeds
+import argparse
 
 def build_model(dataset, device):
     INPUT_DIM = len(dataset.G_FIELD.vocab)
     OUTPUT_DIM = len(dataset.P_FIELD.vocab)
-    HID_DIM = 512
+    HID_DIM = 300
     ENC_LAYERS = 3
     DEC_LAYERS = 3
-    ENC_HEADS = 8
-    DEC_HEADS = 8
-    ENC_PF_DIM = 2048
-    DEC_PF_DIM = 2048
-    ENC_DROPOUT = 0.3
-    DEC_DROPOUT = 0.3
+    ENC_HEADS = 10
+    DEC_HEADS = 10
+    ENC_PF_DIM = 512
+    DEC_PF_DIM = 512
+    ENC_DROPOUT = 0.1
+    DEC_DROPOUT = 0.1
 
-    enc = Encoder(INPUT_DIM, HID_DIM, ENC_LAYERS, ENC_HEADS, ENC_PF_DIM, ENC_DROPOUT, device)
+    enc = Encoder(INPUT_DIM, HID_DIM, ENC_LAYERS, ENC_HEADS, ENC_PF_DIM, ENC_DROPOUT, device, vectors=dataset.G_FIELD.vocab.vectors)
     dec = Decoder(OUTPUT_DIM, HID_DIM, DEC_LAYERS, DEC_HEADS, DEC_PF_DIM, DEC_DROPOUT, device)
 
     G_PAD_IDX = dataset.G_FIELD.vocab.stoi[dataset.G_FIELD.pad_token]
@@ -48,22 +42,60 @@ def build_model(dataset, device):
 
     return model
 
-def main(fpath, batch_size, mode, model_type, model_path):
+def main(args):
+    init_logger(args)
+    set_seeds()
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    dataset = DataLoader(fpath, Librispeech, batch_size, device, model_type)
-
+    dataset = DataLoader(args.data_dir, Librispeech, args.train_batch_size, device, 'transformer', args)
     model = build_model(dataset, device).to(device)
-
-    if mode == 'train':
-        train(dataset, model, 500, model_type)
-    if mode == 'test':
-        test(dataset, model, device, model_path)
-    if mode == 't':
-        test_temp(dataset, model, model_path)
+    
+    if args.do_train:
+        train(dataset, model, args)
+    if args.do_test:
+        test(dataset, model, device, f'checkpoints/{args.version}')
+  
 
 
 if __name__ == '__main__':
-    model_path = 'checkpoints/2020-08-28T21:45:21_transformer'
-    main('data/', 32, 'train', 'transformer', model_path)
+    # model_path = 'checkpoints/2020-09-22T20:29:48_transformer'
+    # main('data/', 64, 'train', 'transformer', model_path)
+
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--test_model_dir", default="./models/third", type=str, help="Path load testing model")
+    parser.add_argument("--save_model_dir", default="./checkpoints/", type=str, help="Path to save trained model")
+
+    parser.add_argument("--data_dir", default="./data/", type=str, help="Train file")
+    parser.add_argument("--train_data_dir", default="librispeech_train-clean.json", type=str, help="Train file")
+    parser.add_argument("--valid_data_dir", default="librispeech_dev-clean.json", type=str, help="valid file")
+    parser.add_argument("--test_data_dir", default="librispeech_test-clean.json", type=str, help="Test file")
+
+    parser.add_argument("--train_batch_size", default=32, type=int, help="Batch size for training.")
+    parser.add_argument("--eval_batch_size", default=64, type=int, help="Batch size for evaluation.")
+
+    parser.add_argument("--learning_rate", default=0.00079, type=float, help="The initial learning rate for Adam.")
+    parser.add_argument("--num_train_epochs", default=500, type=int, help="Total number of training epochs to perform.")
+    parser.add_argument("--weight_decay", default=0.0001, type=float, help="Weight decay if we apply some.")
+    parser.add_argument("--adam_epsilon", default=1e-8, type=float, help="Epsilon for Adam optimizer.")
+    parser.add_argument('--logging_steps', type=int, default=1, help="Log every X updates steps.")
+    parser.add_argument('--save_steps', type=int, default=200, help="Save every X updates steps.")
+    parser.add_argument('--gradient_accumulation_steps', type=int, default=1, help="")
+    parser.add_argument('--max_steps', type=int, default=0, help="Max steps to train")
+    parser.add_argument("--max_grad_norm", default=1.0, type=float, help="Max gradient norm.")
+    parser.add_argument("--max_seq_length", default=128, type=int, help="Max sequence length.")
+    
+    parser.add_argument("--do_train",  action="store_true", help="Whether to run training.")
+    parser.add_argument("--do_test",  action="store_true", help="Whether to run training.")
+    parser.add_argument("--do_lr",  action="store_true", default=False, help="Whether to run training.")
+
+    parser.add_argument("--early_cnt",  type=int, default=3, help="Whether to run training.")
+    parser.add_argument("--version", type=str, default='trained_0.00079', help="Train a sentiment classifier for the model 4")
+    parser.add_argument("--pretrain_vector", type=str, default='GloVe', help="Train a sentiment classifier for the model 4")
+    parser.add_argument("--cuda_num", type=str, default='1', help="Train a sentiment classifier for the model 4")
+
+    args = parser.parse_args()
+
+    main(args)
+
