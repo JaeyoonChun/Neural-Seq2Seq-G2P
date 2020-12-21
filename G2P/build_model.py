9bbb97b02c0333d6ee438cde0c615940717ad532
Jaeyoon Chun <o0ococ@u.sogang.ac.kr>
2020-12-21 22:36:46 +0900

    [#7] Model params initialization
    - param_init 으로 uniform init
    - param_init_xavier 로 transformer 모델 xavier init
    
    Resolves #7

--- G2P/build_model.py
+++ G2P/build_model.py
@@ -1,6 +1,7 @@
 from G2P.encoders import Encoders_list
 from G2P.decoders import Decoders_list
 from G2P.models.model import G2P
+from torch.nn.init import xavier_uniform_
 
 def build_encoder(model_type, model_args, fields, device):
     return Encoders_list[model_type].from_args(model_args, fields, device)
@@ -11,5 +12,13 @@ def build_decoder(model_type, model_args, fields, device):
 def build_model(model_args, opt, fields, device):
     encoder = build_encoder(opt.model_type, model_args, fields, device)
     decoder = build_decoder(opt.model_type, model_args, fields, device)
+    model = G2P(encoder=encoder, decoder=decoder)
     
-    return G2P(encoder=encoder, decoder=decoder)
+    if model_args.param_init != 0.0:
+        for p in model.parameters():
+            p.data.uniform_(-model_args.param_init, model_args.param_init)
+    if model_args.param_init_xavier:
+        for p in model.parameters():
+            if p.dim() > 1:
+                xavier_uniform_(p)
+    return model
