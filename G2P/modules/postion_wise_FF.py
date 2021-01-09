355dc30b675b909089706ec87bdc2fec6cd2fcab
Jaeyoon Chun <o0ococ@u.sogang.ac.kr>
2021-01-09 23:15:45 +0900

    Use LeakyReLU instead of ReLU

--- G2P/modules/postion_wise_FF.py
+++ G2P/modules/postion_wise_FF.py
@@ -7,14 +7,14 @@ class PositionwiseFeedforwardLayer(nn.Module):
         
         self.fc_1 = nn.Linear(hid_dim, pf_dim)
         self.fc_2 = nn.Linear(pf_dim, hid_dim)
-        
+        self.activation = nn.LeakyReLU()
         self.dropout = nn.Dropout(dropout)
         
     def forward(self, x):
         
         #x = [batch size, seq len, hid dim]
         
-        x = self.dropout(torch.relu(self.fc_1(x)))
+        x = self.dropout(self.activation(self.fc_1(x)))
         
         #x = [batch size, seq len, pf dim]
         
