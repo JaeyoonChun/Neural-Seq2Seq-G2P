07a944e457f2403307f7a5c563da4ad0d64542f4
Jaeyoon Chun <o0ococ@u.sogang.ac.kr>
2021-01-09 23:15:19 +0900

    Change weight init ways

--- G2P/build_model.py
+++ G2P/build_model.py
@@ -1,7 +1,7 @@
 from G2P.encoders import Encoders_list
 from G2P.decoders import Decoders_list
 from G2P.models.model import G2P
-from torch.nn.init import xavier_normal_
+from torch.nn.init import kaiming_normal_, xavier_normal_
 
 def build_encoder(model_type, model_args, fields, device):
     return Encoders_list[model_type].from_args(model_args, fields, device)
@@ -14,9 +14,11 @@ def build_model(model_args, opt, fields, device):
     decoder = build_decoder(opt.model_type, model_args, fields, device)
     model = G2P(encoder=encoder, decoder=decoder)
     
-    if model_args.param_init != 0.0:
+    if model_args.param_init_he:
         for p in model.parameters():
-            p.data.normal_(0, model_args.param_init)
+            if p.dim() > 1:
+                kaiming_normal_(p)
+    
     if model_args.param_init_xavier:
         for p in model.parameters():
             if p.dim() > 1:
